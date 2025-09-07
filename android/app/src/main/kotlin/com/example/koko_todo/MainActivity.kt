09aaa8b5608624e6f423ce975eb4348f1158d0e2
finesse-codes <hello@finesse.dev>
2025-09-07 21:27:37 +1000

    input controller

---
+++ android/app/src/main/kotlin/com/example/koko_todo/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.koko_todo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:koko_todo/pages/todo_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: ToDoPage());
+  }
+}
